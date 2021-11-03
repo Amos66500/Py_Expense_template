6fc178b7e09dd9d349a85281435a591ba86acec9
Jean-Remi Avargues <jean-remi>
2021-11-03 21:21:07 +0100

    [NLPF][TP] First commit

--- expense.py
+++ expense.py
@@ -1,4 +1,15 @@
 from PyInquirer import prompt
+import csv
+
+
+# Function who open the users.csv file and give a list of users for the spender question
+def get_users():
+    options = []
+    with open('users.csv', newline='') as users_csv_file:
+        reader = csv.reader(users_csv_file, delimiter=' ')
+        for row in reader:
+            options.append(row[0])
+    return options
 
 expense_questions = [
     {
@@ -12,18 +23,26 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": get_users(),
     },
-
 ]
-
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    # Here we create a row from the dictionnary 'infos' to get the value from each key
+    row_infos = []
+    for key, value in infos.items():
+        row_infos.append(value)
+
+    # Here we open the csv file in append mode and we add a row with the row_infos data
+    with open('expense_report.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(row_infos)
+
     print("Expense Added !")
     return True
 
--- user.py
+++ user.py
@@ -1,8 +1,25 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+        {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    # Here we create a row from the dictionnary 'infos' to get the value from each key
+    row_infos = []
+    for key, value in infos.items():
+        row_infos.append(value)
+    
+    # Here we open the csv file in append mode and we add a row with the row_infos data
+    with open('users.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(row_infos)
+
+    print("User Added !")
+    return True
